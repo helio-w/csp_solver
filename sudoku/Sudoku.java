b0d6eea8a14e7574dfea03fd4d3e6b3bfa215181
Maliochka <maeld@laposte.net>
2024-03-17 14:27:53 +0100

    optimisations et simplifications

--- sudoku/Sudoku.java
+++ sudoku/Sudoku.java
@@ -4,15 +4,33 @@ import var.IntCsp;
 
 import java.util.ArrayList;
 
+import solver.Solver;
+
 public class Sudoku{
     public ArrayList<ArrayList<IntCsp>> sudokuGrid;
+    public Solver solver;
 
+    /**
+     * Constructeur de la grille de Sudoku.<br><br>
+     * Les variables CSP sont automatiquement ajoutees.
+     */
     public Sudoku() {
         this.sudokuGrid = new ArrayList<ArrayList<IntCsp>>();
+        this.solver = new Solver();
         for(int i = 0; i < 9; i++) {
             this.sudokuGrid.add(new ArrayList<IntCsp>());
             for(int j = 0; j < 9; j++) {
-                this.sudokuGrid.get(i).add(null);
+                String name = "var_" + i + "_" + j;
+                try
+                {   
+                    IntCsp currVar = new IntCsp(name, 1, 9);
+                    this.sudokuGrid.get(i).add(currVar);
+                    this.solver.addVariable(currVar);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
             }
         }
     }
@@ -28,35 +46,18 @@ public class Sudoku{
     }
 
     /**
-     * Ajoute une valeur fixe à la case (x, y)
+     * Fixe la variable correspondant à la case (x, y) avec la valeur val
      * @param x : coordonnée x de la case
      * @param y : coordonnée y de la case
-     * @param v : valeur à fixer
-     * @param name : nom de la variable
+     * @param val : valeur à affecter à la variable
      */
-    public void setCell(int x, int y, int v, String name) {
-        try
-        {
-            IntCsp var = new IntCsp(name, 1, 9);
-            var.setUniqueVal(v);
-            this.sudokuGrid.get(x).set(y, var);
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
+    public void setCell(int x, int y, int val) {
+        this.sudokuGrid.get(x).get(y).setUniqueVal(val);
     }
 
     /**
-     * Met une variable IntCsp à la case (x, y)
-     * @param x : coordonnée x de la case
-     * @param y : coordonnée y de la case
-     * @param v : variable à mettre
+     * Affiche le grille de sudoku en mode console
      */
-    public void setCellVar(int x, int y, IntCsp v) {
-        this.sudokuGrid.get(x).set(y, v);
-    }
-
     public void displayGrid() {
         for (int i = 0; i < 9; i++) {
             if (i % 3 == 0 && i != 0) {
@@ -64,7 +65,7 @@ public class Sudoku{
             }
             for (int j = 0; j < 9; j++) {
                 IntCsp var = this.getCell(i, j);
-                if (var != null) {
+                if (var.isFixed()) {
                     int val = var.getValue();
                     System.out.print(val + " ");
                 } else {
@@ -80,11 +81,13 @@ public class Sudoku{
 
     public static void main(String[] args) {
         Sudoku sudoku = new Sudoku();
-        sudoku.setCell(0, 0, 1, "a");
-        sudoku.setCell(1, 1, 2, "b");
-        sudoku.setCell(2, 2, 3, "c");
-        sudoku.setCell(0, 2, 4, "d");
-        sudoku.setCell(0, 4, 5, "e");
+
+        sudoku.setCell(0, 0, 1);
+        sudoku.setCell(1, 1, 2);
+        sudoku.setCell(2, 2, 3);
+        sudoku.setCell(0, 2, 4);
+        sudoku.setCell(0, 4, 5);
+
         sudoku.displayGrid();
     }
 }
